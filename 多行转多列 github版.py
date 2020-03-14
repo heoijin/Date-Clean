123e47d9e81e57fe6f289c62c507986fee2b01ab
heoijin <heoijin@163.com>
2020-03-14 11:50:44 +0800

    优化拼接模块

--- 多行转多列 github版.py
+++ 多行转多列 github版.py
@@ -1,5 +1,6 @@
-import pandas as pd
 import re
+
+import pandas as pd
 
 
 # <editor-fold desc="案例模块">
@@ -9,7 +10,9 @@ def open_file(sheets_name):
 
 def get_new_columns(df):
     '''
-    利用正则匹配出目标columns
+    >> 利用正则匹配出目标columns
+    >> df.columns=['栋数', '户型', '姓名', '性别', '电话', '房号', '姓名.1', '性别.1', '电话.1', '房号.1','姓名.2', '性别.2', '电话.2', '房号.2']
+    >> 当我们的columns重复的时候，pandas会自动帮我们在重复的columns后面加上.编号。
     :param df:
     :return:
     '''
@@ -17,11 +20,11 @@ def get_new_columns(df):
     pattern_name=r',(姓名.?\d?),'
     pattern_room=r',(房号.?\d?),'
     pattern_phoone=r',(电话.?\d?),'
+    # 通过前后增加逗号，确保正则的精准匹配
     columns_str=','+','.join(df.columns.to_list())+','
     columns_name=re.findall(pattern_name,columns_str)
     columns_room=re.findall(pattern_room,columns_str)
     columns_phone=re.findall(pattern_phoone,columns_str)
-    #对新的columns合并和排序
     target_columns=[]
     # 将每一行的数据变为一个一层嵌套的列表
     for i in range(len(columns_name)):
@@ -35,12 +38,14 @@ def merge_cols(Series):
     # 获取当行所有数据
     value=Series.values
     result=[]
+    # 将每一行的数据变为一个一层嵌套的列表
     for idx in range(0,len(value),3):
         result.append([value[idx],value[idx+1],value[idx+2]])
     return result
 
 def rebuild_df(df,merge_columns):
     # 获取表格头部通用信息
+	# merge_columns=['房号', '姓名', '电话', '房号.1', '姓名.1', '电话.1', '房号.2', '姓名.2', '电话.2']
     df_new=df.iloc[:,:2]
     # 对数据进行合并
     df_new['merge']=df.loc[:,merge_columns].apply(merge_cols,axis=1)
